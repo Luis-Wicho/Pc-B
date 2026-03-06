6a511ee077560ac3d98d495adb249ae402a6b5e4
M19ichelle <arlettebisoso@gmail.com>
2026-03-06 15:44:32 -0600

    cambios de orden de pago

---
+++ modules/ordenes_pago/domain/ordenes_pago.repository.ts
@@ -0,0 +1,28 @@
+// Importamos la entidad OrdenPago que define la estructura de los datos
+import { ordenes_pago } from "./ordenes_pago.entity";
+
+// Definimos la interfaz del repositorio.
+// Esta interfaz funciona como un contrato que indica qué operaciones
+// se pueden realizar con las órdenes de pago.
+export interface OrdenPagoRepository {
+
+  // Método para obtener todas las órdenes de pago registradas.
+  // Devuelve una promesa que contiene un arreglo de órdenes de pago.
+  getAll(): Promise<ordenes_pago[]>;
+
+  // Método para crear una nueva orden de pago.
+  // Recibe un objeto de tipo OrdenPago y devuelve
+  // una promesa con la orden de pago creada.
+  create(ordenPago:ordenes_pago): Promise<ordenes_pago>;
+
+  // Método para actualizar una orden de pago existente.
+  // Recibe la orden de pago con los datos actualizados
+  // y devuelve una promesa con la orden de pago actualizada.
+  update(ordenPago: ordenes_pago): Promise<ordenes_pago>;
+
+  // Método para buscar una orden de pago por su ID.
+  // Recibe el id de la orden de pago y devuelve:
+  // - la orden de pago si existe
+  // - null si no se encuentra en la base de datos.
+  findById(id: number): Promise<ordenes_pago| null>;
+}
