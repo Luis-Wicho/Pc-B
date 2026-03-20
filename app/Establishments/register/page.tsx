f63e44b94d07c02369db096e85130570b0bc2b47
Luis-Wicho <sanchezlopezluisangel07@gmail.com>
2026-03-20 17:15:31 -0600

    Avance del modulo Establishments

---
+++ app/Establishments/register/page.tsx
@@ -0,0 +1,5 @@
+import RegistrarEstablecimientoView from "@/modules/Establishments/presentation/views/registerEstablishmentView";
+
+export default function Page() {
+    return <RegistrarEstablecimientoView/>
+}
